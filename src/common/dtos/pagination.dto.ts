c19266df427f1a36db66fb76b8e59173c9ffcea1
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-11 10:02:35 -0400

    [V0.1.5] feat: agregada la logica de paginacion del GET /products

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from "class-transformer";
+import { IsOptional, IsPositive, Min } from "class-validator";
+
+export class PaginationDto{
+
+    @IsOptional()
+    @IsPositive()
+    @Type(() => Number) // Es lo mismo que hacer enableImplicitConversions: True
+    limit?:number;
+
+    @IsOptional()
+    @Type(() => Number)
+    @Min(0)
+    offset?:number;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -34,9 +35,14 @@ export class ProductsService {
 
 
 
-  // TODO: Paginacion
-  findAll() {
-    return this.productRepository.find({});
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 100, offset = 0 } = paginationDto;
+
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+      // TODO: Relaciones
+    });
   }
 
   async findOne(id: string) {
