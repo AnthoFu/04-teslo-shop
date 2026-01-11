af4233b05efe49fc7aa674e139dd632f852b2274
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-10 21:19:43 -0400

    [V0.1.2] feat: creada la logica inicial del POST /api/products y creado handleDBExceptions basico

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  private readonly logger = new Logger('ProductsService')
+
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    
+
+  ){}
+
+
+  async create(createProductDto: CreateProductDto) {
+
+    try{
+      const product = this.productRepository.create(createProductDto)
+
+      await this.productRepository.save(product);
+
+    } catch (error){
+      this.handleDBExceptions(error);
+    }
+    
   }
+
+
+
 
   findAll() {
     return `This action returns all products`;
@@ -22,5 +49,14 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+
+  private handleDBExceptions(error){
+    if (error.code === '23505'){
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(error)
+    throw new InternalServerErrorException('Ha ocurrido un error interno inesperado. Por favor revisa los logs.')
   }
 }
