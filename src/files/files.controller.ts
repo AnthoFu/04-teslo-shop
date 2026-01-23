8165caa0d57a40a43efff4eaaae78f34f42db23f
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-23 15:50:47 -0400

    [V0.3.3] feat: Agregado filtro para solo obtener imagenes

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors( FileInterceptor('file') )
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter
+  }) )
 
   uploadProductFile( 
     @UploadedFile() file: Express.Multer.File,
   ){
-    return file;
+
+    if (!file){
+      throw new BadRequestException('Asegurate de que el archivo este en un formato de imagen valido.')
+    }
+
+    return {
+      fileName: file.originalname
+    }
   }
 }
---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,18 @@
+export const fileFilter = (request: Express.Request, file:Express.Multer.File, callback: Function )=> {
+
+    console.log(file)
+
+    if (!file ) {
+        return callback( new Error('El archivo esta vacio'), false);
+    }
+
+    const fileExtension = file.mimetype.split('/')[1]; // para obtener la extension (ej: jpg, png, gif)
+    const validExtensions = ['jpg', 'jpeg', 'png', 'gif']
+
+
+    if ( validExtensions.includes(fileExtension)){
+        return callback(null, true)
+    }
+    
+    callback(null, false);
+}
