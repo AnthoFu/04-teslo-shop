9ed15af9bcd98acf3e57689685aa63c639a471a3
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-26 16:55:47 -0400

    [V0.4.8] feat: Logica inicial de estrategias de JWT

---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface JwtPayload {
+    email: string;
+
+    //TODO: agregar lo que se quiera grabar
+}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,42 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "../entities/user.entity";
+import { JwtPayload } from "../interfaces/jwt-payload.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ConfigService } from "@nestjs/config";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy (Strategy){
+    
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+
+        private readonly configService: ConfigService
+    ){
+        super({
+            secretOrKey: configService.get<string>('JWT_SECRET')!,
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        });
+    }
+
+    async validate( payload: JwtPayload): Promise<User>{
+        
+        const {email} = payload;
+
+        const user = await await this.userRepository.findOneBy({
+            email
+        });
+
+        if (!user){
+            throw new UnauthorizedException('Token no encontrado')
+        }
+
+        if (!user.isActive){
+            throw new UnauthorizedException('El usuario esta inactivo, comuniquese con un administrador')
+        }
+        return user;
+    }
+}
