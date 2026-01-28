9a2a7c05ceddf7751c713978e6587ba18f84d997
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-28 10:18:38 -0400

    [V0.4.12] feat: creacion de decorador custom para los encabezados

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Headers } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
+import { GetUser, RawHeaders } from './decorators';
 import { User } from './entities/user.entity';
+import * as http from 'http';
 
 
 @Controller('auth')
@@ -23,14 +24,21 @@ export class AuthController {
     @Get('private')
     @UseGuards(AuthGuard())
     testingPrivateRoutes(
-      @GetUser() user:User){
-      //@Req() request: Express.Request)
-      console.log({user})
+      //@Req() request: Express.Request,
+      @GetUser() user:User,
+      @GetUser('email') userEmail: string,
+      @RawHeaders() rawHeaders: string,
+      @Headers() headers:http.IncomingHttpHeaders
+    ){
+      
       
       return {
         ok:true,
         message:'hola mundo',
-        user
+        user,
+        userEmail,
+        rawHeaders,
+        headers
       }
     }
   }
