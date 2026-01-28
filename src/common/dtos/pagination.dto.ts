2e0a51df7d8af28da8a2ecee13e2952102c0c6a1
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-28 15:21:01 -0400

    [V0.6.3] feat: agregada documentacion de los DTOs en el swagger

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto{
 
+    @ApiProperty({
+        default:10, description:'Cantidad de valores a traer'
+    })
     @IsOptional()
     @IsPositive()
     @Type(() => Number) // Es lo mismo que hacer enableImplicitConversions: True
     limit?:number;
 
+    @ApiProperty({
+        default:0, description:'Cantidad de valores a saltar'
+    })
     @IsOptional()
     @Type(() => Number)
     @Min(0)
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, 
     IsIn, 
     IsInt, 
@@ -10,41 +11,54 @@ import { IsArray,
 
 export class CreateProductDto {
 
+    @ApiProperty({
+        description:'Titulo de producto (unico)',
+        nullable:false,
+        minLength:1
+    })
     @IsString()
     @MinLength(1)
-
     title: string;
 
+    @ApiProperty()
     @IsNumber()
     @IsPositive()
     @IsOptional()
     price?: number;
 
+    @ApiProperty()
     @IsString()
     @IsOptional()
     description?:string;
 
+    @ApiProperty()
     @IsString()
     @IsOptional()
     slug?: string;
 
+
+    @ApiProperty()
     @IsInt()
     @IsPositive()
     @IsOptional()
     stock?: number;
 
+    @ApiProperty()
     @IsString({each: true})
     @IsArray()
     sizes: string[];
 
+    @ApiProperty()
     @IsIn(['hombre', 'mujer', 'niño', 'niña', 'unisex'])
     gender: string;
 
+    @ApiProperty()
     @IsArray()
     @IsString({each:true})
     @IsOptional()
     tags?: string[];
 
+    @ApiProperty()
     @IsArray()
     @IsString({each:true})
     images?: string[];
