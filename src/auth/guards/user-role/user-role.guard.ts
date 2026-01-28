e399ab1300941e4951aa4dbceee035d0d71c1507
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-28 10:44:52 -0400

    [V0.4.14] feat: creacion de guard para roles

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -17,6 +18,31 @@ export class UserRoleGuard implements CanActivate {
 
     const validRoles: string[] = this.reflector.get('roles', context.getHandler())
     console.log('Roles validos:', {validRoles})
-    return true;
+
+
+    if (!validRoles){
+      return true;
+    }
+    
+    if (validRoles.length === 0){
+      return true
+    }
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user){
+      throw new BadRequestException(`El usuario no se ha encontrado`)
+    }
+
+    console.log('[UserRoleGuard] Roles del usuario:',{userRoles: user.roles})
+    
+    for (const role of user.roles){
+      if (validRoles.includes(role)){
+        return true;
+      }
+    }
+    
+    throw new ForbiddenException(`El usuario ${user.fullName} necesita un rol de administrador valido. Ej: [${validRoles}]`)
   }
 }
