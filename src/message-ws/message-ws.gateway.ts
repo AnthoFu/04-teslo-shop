80761ff873df128e6c2e6653120698040beefbf3
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-29 15:56:29 -0400

    [V0.6.7] feat: agregada identificacion de clientes

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -2,13 +2,22 @@ import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nes
 import { MessageWsService } from './message-ws.service';
 import { Socket } from 'socket.io';
 
+
+
 @WebSocketGateway({cors: true, namespace:'/'})
 export class MessageWsGateway  implements OnGatewayConnection, OnGatewayDisconnect{
   constructor(private readonly messageWsService: MessageWsService) {}
   handleDisconnect(client: Socket) {
-    console.log('Cliente conectado', client.id);
+    // console.log('Cliente conectado', client.id);
+
+    this.messageWsService.registerClient(client);
+  
+    console.log('Clientes conectados:', this.messageWsService.getConnectedClients())
   }
   handleConnection(client: Socket, ...args: any[]) {
-    console.log('Cliente desconectado', client.id);
+    // console.log('Cliente desconectado', client.id);
+
+    this.messageWsService.removeClient(client.id);
   }
+
 }
