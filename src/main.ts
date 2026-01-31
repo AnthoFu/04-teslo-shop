f6dd897d0e8f433acef06ecb8d3c6ebeb3fdc44a
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-31 10:41:56 -0400

    [V1.0.2] fix: Habilitar CORS para permitir solicitudes desde el frontend

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api')
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
